6c21d273c811e5450ada8fbf7c071159e6a4f27c
NickChapman1988 <muppetnj_uk@hotmail.com>
2022-08-03 13:37:05 +0000

    Add Enemy class, with draw and update methods

--- script.js
+++ script.js
@@ -9,24 +9,36 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-// factory function to produce multiple NPC elements
-
-enemy1 = { 
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
+class Enemy {
+    // mandatory constructor method for JS classes
+    constructor(){
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
+    update(){
+        // increase x and y coordinate with each loop
+        this.x++;
+        this.y++;
+    }
+    draw(){
+        // draw enemy using properties of Enemy class
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
+
+// variable to create enemy using Enemy class
+const enemy1 = new Enemy();
 
 // custom animation loop
 function animate(){
     // clear old paint!
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // increase x and y coordinate with each loop
-    enemy1.x++;
-    enemy1.y++;
-    // draw enemy using x,y and width, height values
-    ctx.fillRect(enemy1.x, enemy.y, enemy1.width, enemy1.height);
+    // call update() method of Enemy class
+    enemy1.update();
+    // call draw() method of Enemy class
+    enemy1.draw();
     requestAnimationFrame(animate);
 }
 animate();
