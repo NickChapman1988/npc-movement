e5505251ea51d71fd909a4a1f36e3d521f2157be
NickChapman1988 <muppetnj_uk@hotmail.com>
2022-08-03 13:48:56 +0000

    Createeloops to generate enemies array, and animate multiple NPCs

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
 // set height and width to match values set in CSS
 CANVAS_WIDTH = canvas.width = 500;
@@ -29,16 +31,24 @@ class Enemy {
 }
 
 // variable to create enemy using Enemy class
-const enemy1 = new Enemy();
+
+
+// loop to generate multiple NPCs
+for (let i = 0; i < numberOfEnemies; i++){
+    /* loops through number of enemies, 
+    uses Enemy class to create NPC, 
+    pushes new NPC to enemies Array */
+    enemiesArray.push(new Enemy());
+}
 
 // custom animation loop
 function animate(){
     // clear old paint!
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // call update() method of Enemy class
-    enemy1.update();
-    // call draw() method of Enemy class
-    enemy1.draw();
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
     requestAnimationFrame(animate);
 }
 animate();
